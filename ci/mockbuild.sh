dc4ceafe3c9b06ef40e3fdbf02944ed4d585de23
Charles Lane <lane@dchooz.org>
2024-11-21 08:52:03 -0500

    prepare for CI/CD

--- ci/mockbuild.sh
+++ ci/mockbuild.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-NAME=pnp4nagios
+NAME=pgplot
 VERSION=$(grep '^Version: ' ${NAME}.spec | cut -d ':' -f2 | awk -F'%' '{print $1}' | tr -d ' ')
 RELEASE=$(grep '^Release: ' ${NAME}.spec | cut -d ':' -f2 | awk -F'%' '{print $1}' | tr -d ' ')
 
 dnf install -y epel-release
 dnf install -y mock
 
-cp ${NAME}.spec ${NAME}.spec.base
-
 echo "VERSION = ${VERSION}"
 echo "RELEASE = ${RELEASE}"
 mkdir outputs
+cp ../${NAME}.spec ${NAME}.spec.base
 
-# want to use the plain-vanilla pnp4nagios.spec so that
+# want to use the plain-vanilla pgplot.spec so that
 # it's the one that is included in the srpm
 
 BREL="${RELEASE}.alma%{?dist}"
@@ -22,10 +21,13 @@ Release:        ${BREL}" <${NAME}.spec.base >${NAME}.spec
 
 config='alma+epel-8-x86_64'
 mock -v -r $config  \
-     --additional-package=selinux-policy-targeted \
-     --additional-package=selinux-policy-devel \
-     --additional-package=perl-Time-HiRes \
-     --additional-package=rrdtool \
+     --additional-package=libpng-devel \
+     --additional-package=tk-devel \
+     --additional-package=libX11-devel \
+     --additional-package=gcc-gfortran \
+     --additional-package=perl \
+     --additional-package=glibc-common \
+     --additional-package=openssl \
      --spec=${NAME}.spec \
      --sources=${NAME}-${VERSION}.tgz \
      --resultdir=./outputs -N
@@ -33,10 +35,13 @@ mock -v -r $config  \
 cp ${NAME}.spec.base ${NAME}.spec
 config='fedora-38-x86_64'
 mock -v -r $config \
-     --additional-package=selinux-policy-targeted \
-     --additional-package=selinux-policy-devel \
-     --additional-package=perl-Time-HiRes \
-     --additional-package=rrdtool \
+     --additional-package=libpng-devel \
+     --additional-package=tk-devel \
+     --additional-package=libX11-devel \
+     --additional-package=gcc-gfortran \
+     --additional-package=perl \
+     --additional-package=glibc-common \
+     --additional-package=openssl \
      --spec=${NAME}.spec \
      --sources=${NAME}-${VERSION}.tgz --resultdir=./outputs -N
 
